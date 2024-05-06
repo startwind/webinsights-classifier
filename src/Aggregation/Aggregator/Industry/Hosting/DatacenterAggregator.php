698e08b2798c1a197e63ff9def5b2e57b592e761
Nils Langner <nils.langner@startwind.io>
2024-05-06 12:22:26 +0200

    datacenter aggregator

---
+++ src/Aggregation/Aggregator/Industry/Hosting/DatacenterAggregator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Startwind\WebInsights\Aggregation\Aggregator\Industry\Hosting;
+
+use Startwind\WebInsights\Aggregation\Aggregator\CountingAggregator;
+use Startwind\WebInsights\Classification\ClassificationResult;
+use Startwind\WebInsights\Classification\Classifier\Url\IPLocationClassifier;
+
+class DatacenterAggregator extends CountingAggregator
+{
+    protected int $limit = 100;
+
+    public function aggregate(ClassificationResult $classificationResult): void
+    {
+        $as = $classificationResult->getTagsStartingWithString(IPLocationClassifier::TAG_HOSTING_LOCATION_PREFIX . 'as', true, true);
+
+        if (count($as) === 1) {
+            $this->increaseCount($as);
+        }
+    }
+}
