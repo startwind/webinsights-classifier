e0c890ec6d352bc61817b81f03a981d823646c27
Nils Langner <nils.langner@startwind.io>
2024-05-31 11:18:30 +0200

    Update import_asn.php

--- tools/import_asn.php
+++ tools/import_asn.php
@@ -16,6 +16,8 @@ $blockSize = 500;
 
 $export = new \Startwind\WebInsights\Hosting\Export\ApiExporter();
 
+$count = 0;
+
 // Loop through the directory
 foreach ($iterator as $fileInfo) {
     // Skip . and ..
@@ -25,15 +27,15 @@ foreach ($iterator as $fileInfo) {
 
     // Print the current item's name
     if ($fileInfo->isDir()) {
+        $count++;
 
         $as = $fileInfo->getFilename();
 
         $asInfo = json_decode(file_get_contents($dir . '/' . $as . '/aggregated.json'), true);
 
         $export->export($as, $asInfo['subnets']['ipv4']);
-        var_dump($asInfo);
 
-        die;
+        echo "# $count | " . $asInfo . "\n";
     }
 
 }
