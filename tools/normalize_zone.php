950eed5cd01985b0c695eb72822ecf5ab6e30677
Nils Langner <nils.langner@startwind.io>
2024-05-31 16:50:46 +0200

    Create normalize_zone.php

---
+++ tools/normalize_zone.php
@@ -0,0 +1,25 @@
+<?php
+
+$from = $argv[1];
+$to = $argv[2];
+
+$fromHandle = fopen($from, 'r');
+$toHandle = fopen($to, 'w');
+
+$count = 0;
+
+while ($data = fgetcsv($fromHandle)) {
+    $count++;
+
+    $ip = ip2long($data[2]);
+
+    if ($ip) {
+        $normalized = [
+            'ip' => $ip,
+            'domain' => $data[0],
+            'discovery_date' => $data[11]
+        ];
+
+        fputcsv($toHandle, $normalized);
+    }
+}
